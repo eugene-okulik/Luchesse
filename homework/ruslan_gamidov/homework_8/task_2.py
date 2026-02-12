86d80c2eafd74562e76d229dc657f28406ef832d
Luchesse <darkcarl@yandex.ru>
2026-02-12 19:20:48 +0300

    homework_8 Done v.1

---
+++ homework/ruslan_gamidov/homework_8/task_2.py
@@ -0,0 +1,17 @@
+def fibo():
+    x, y = 0, 1
+    i = 0
+    while True:
+        i += 1
+        yield x, i
+        x, y = y, x + y
+
+data = [5, 200, 1000, 100000]
+index = 0
+
+for xx, ii in fibo():
+    if ii == data[index]:
+        print(xx)
+        index += 1
+        if index == len(data):
+            break
