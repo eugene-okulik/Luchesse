497528eb953ef400d54006c147d303d787565d0b
Luchesse <darkcarl@yandex.ru>
2026-02-17 23:22:59 +0300

    homework_9 Done v.1

---
+++ homework/ruslan_gamidov/homework_9/task_2.py
@@ -0,0 +1,12 @@
+temperatures = [20, 15, 32, 34, 21, 19, 25, 27, 30, 32, 34, 30, 29, 25, 27, 22, 22, 23, 25, 29, 29, 31, 33, 31, 30, 32,
+                30, 28, 24, 23]
+
+
+def hot(i):
+    return i > 28
+
+
+hot_list = list(filter(hot, temperatures))
+
+print(hot_list)
+print(min(hot_list))
